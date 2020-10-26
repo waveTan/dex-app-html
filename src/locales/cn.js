4fcf305542f92f850bbb5ff0c12472060e3f8f26
wave <wave98688@gmail.com>
2020-10-26 15:49:23 +0800

    开始

---
+++ src/locales/cn.js
@@ -0,0 +1,17 @@
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+
+const cn = {
+  "public": {
+    "confirm": "确 定",
+    "cancel": "取 消",
+    "copy": "复制",
+    "copyComplete": "复制完成",
+    "address": "地址",
+    "balance": "余额",
+    "": "",
+  },
+
+  ...zhLocale
+};
+
+export default cn
---
+++ src/locales/en.js
@@ -0,0 +1,16 @@
+import enLocale from 'element-ui/lib/locale/lang/en'
+
+const en = {
+  "public": {
+    "confirm": "Confirm",
+    "cancel": "Cancel",
+    "copy": "Copy",
+    "copyComplete": "Copy complete",
+    "address": "Address",
+    "balance": "Balance",
+    "": "",
+  },
+  ...enLocale
+
+};
+export default en
